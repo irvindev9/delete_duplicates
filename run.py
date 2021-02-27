f4470086eb35f6717fdd0434c7bb9214e6daa653
Irvin López <irvin_21_95@hotmail.com>
2021-02-26 21:04:07 -0700

    min version

--- run.py
+++ run.py
@@ -2,24 +2,11 @@ f = open("../../output/NewGlobal.txt", "r")
 o = open("../../output/NewGlobalwithoutduplicates.txt","w")
 
 exist = []
-dup = []
-cont = 0
+
 for x in f:
     if x.replace("\n","") not in exist:
         exist.append(x.replace("\n",""))
         o.write(x)
-    else:
-        dup.append(x.replace("\n",""))
-        print(x)
-        
-d = open("duplicates.txt","w")
-
-print(exist)
-print(dup)
-
-for x in exist:
-    d.write(x)
 
 o.close()
 f.close()
-d.close()
